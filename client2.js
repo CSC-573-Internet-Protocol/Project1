c79a70d6a109ef20310ab1d8354f269b0c5ec2a7
MakarandPundlik <mmp2308@gmail.com>
2025-03-06 19:49:48 -0500

    added apoplication layer dat ain http2 as well

--- client2.js
+++ client2.js
@@ -3,13 +3,14 @@ import { getStats } from "./statistics.js";
 
 let client = http2.connect("http://localhost:3002", { keepAlive: true });
 
-const fetchFile = async (filePath, transmissions) => {
+const fetchFile = async (fileData) => {
+  const { path, transmissions } = fileData;
   return new Promise((resolve, reject) => {
     if (client.closed) {
       client = http2.connect("http://localhost:3002", { keepAlive: true });
     }
 
-    const req = client.request({ ":path": filePath });
+    const req = client.request({ ":path": path });
     let data = "";
     const start = process.hrtime.bigint();
     req.setEncoding("utf8");
@@ -22,6 +23,7 @@ const fetchFile = async (filePath, transmissions) => {
       const end = process.hrtime.bigint();
       const duration = Number(end - start) / 1e6;
       transmissions.push(duration);
+      fileData.totalAppLayerbits = data.length;
       resolve();
     });
 
@@ -34,10 +36,11 @@ const fetchFile = async (filePath, transmissions) => {
   });
 };
 
-const fetchFiles = async ({ path, iterations, transmissions }) => {
+const fetchFiles = async (fileData) => {
+  const { path, iterations } = fileData;
   for (let i = 0; i < iterations; i++) {
     try {
-      await fetchFile(path, transmissions);
+      await fetchFile(fileData);
     } catch (error) {
       console.error(`Error fetching ${path} (iteration ${i}):`, error);
     }
@@ -48,16 +51,24 @@ const apiCalls = async () => {
   for (const item of configurations) {
     await fetchFiles(item);
   }
-
-  // Close the client only after all requests are done
   client.close();
 };
 
 const configurations = [
-  { path: "/A_10kB", iterations: 1000, transmissions: [] },
-  { path: "/A_100kB", iterations: 100, transmissions: [] },
-  { path: "/A_1MB", iterations: 10, transmissions: [] },
-  { path: "/A_10MB", iterations: 1, transmissions: [] },
+  {
+    path: "/A_10kB",
+    iterations: 1000,
+    transmissions: [],
+    totalAppLayerbits: 0,
+  },
+  {
+    path: "/A_100kB",
+    iterations: 100,
+    transmissions: [],
+    totalAppLayerbits: 0,
+  },
+  { path: "/A_1MB", iterations: 10, transmissions: [], totalAppLayerbits: 0 },
+  { path: "/A_10MB", iterations: 1, transmissions: [], totalAppLayerbits: 0 },
 ];
 
 await apiCalls();
